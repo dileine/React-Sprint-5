e95e6f30e836b70c64649dc864d02ef81e7b499c
dileine <mar.audiovisuals@gmail.com>
2023-04-23 14:47:15 +0200

    EX1: get data from the API

---
+++ ts/main.ts
@@ -0,0 +1,22 @@
+let joke: string;
+const button = document.querySelector('.button-joke');
+
+const getJoke = async () => 
+{
+    try{
+    const response = await fetch('https://icanhazdadjoke.com/', 
+    {headers: {
+        Accept: 'application/json'
+    }
+});
+    const joke = await response.json();
+       return joke.joke;
+    } catch(err)
+    {console.error("sorry there was an error, try again")};
+    
+};
+
+button?.addEventListener('click', async()=>{
+    const joke = await getJoke();
+    console.log(joke);
+});
